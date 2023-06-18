dd6f473c6147efdcecbf292da4940e09fa87d0f5
Nguyễn Xuân Nhân <nguyenxuannhan407@gmail.com>
2023-06-19 00:25:18 +0700

    use pure php

--- Controllers/CoffeeController.php
+++ Controllers/CoffeeController.php
@@ -4,14 +4,10 @@ declare(strict_types=1);
 
 namespace Hutech\Controllers;
 
+use Hutech\Factories\CategoryFactory;
 use Hutech\Factories\ProductFactory;
 use Hutech\Services\CategoryService;
 use Hutech\Services\ProductService;
-use Nette\Utils\FileSystem;
-use Nette\Utils\Image;
-use Nette\Utils\ImageException;
-use Nette\Utils\UnknownImageFileException;
-use Nette\Utils\Validators;
 
 include_once './Services/ProductService.php';
 include_once './Services/CategoryService.php';
@@ -25,7 +21,8 @@ readonly class CoffeeController
     public function __construct(
         protected ProductService $coffeeService,
         protected ProductFactory $coffeeFactory,
-        protected CategoryService $categoryService
+        protected CategoryService $categoryService,
+        protected CategoryFactory $categoryFactory
     )
     {
     }
@@ -43,10 +40,6 @@ readonly class CoffeeController
         require_once './Views/Coffee/Add.php';
     }
 
-    /**
-     * @throws ImageException
-     * @throws UnknownImageFileException
-     */
     public function insert(): void
     {
         if (isset($_POST['submit'])) {
@@ -92,10 +85,6 @@ readonly class CoffeeController
         }
     }
 
-    /**
-     * @throws ImageException
-     * @throws UnknownImageFileException
-     */
     public function update(): void
     {
         if (!isset($_POST['submit'])) {
@@ -114,7 +103,7 @@ readonly class CoffeeController
 
         if ($_FILES['Image']['name']) {
             if ($imgPath && file_exists($this::FILE_PATH . $coffee->image)) {
-                FileSystem::delete($this::FILE_PATH . $coffee->image);
+                unlink($coffee->image);
             }
 
             $imgPath = $this->uploadImage($_FILES['Image']) ?? '';
@@ -149,7 +138,7 @@ readonly class CoffeeController
 
         if ($coffee) {
             if ($coffee->image && file_exists($this::FILE_PATH . $coffee->image)) {
-                FileSystem::delete($this::FILE_PATH . $coffee->image);
+                unlink($coffee->image);
             }
             $this->coffeeService->delete((int) $_GET['id']);
         }
@@ -157,14 +146,10 @@ readonly class CoffeeController
         header('Location: /hutech-coffee/manager');
     }
 
-    /**
-     * @throws ImageException
-     * @throws UnknownImageFileException
-     */
     private function uploadImage(mixed $image): ?string
     {
         if (!is_dir($this::FILE_PATH)) {
-            FileSystem::createDir($this::FILE_PATH);
+            mkdir($this::FILE_PATH, 0777, true);
         }
 
         if (!getimagesize($_FILES["Image"]["tmp_name"])) {
@@ -190,9 +175,7 @@ readonly class CoffeeController
         $fileName = uniqid('hutech-', true) . '.' . $fileExtension;
 
         if (empty($errors)) {
-            $image = Image::fromFile($fileTemp);
-            $image->resize(250, 250, Image::ShrinkOnly);
-            $image->save($this::FILE_PATH . $fileName, 70);
+            move_uploaded_file($fileTemp, $this::FILE_PATH . $fileName);
         } else {
             $_SESSION['image_error'] = $errors;
             return null;
@@ -203,20 +186,29 @@ readonly class CoffeeController
 
     private function validation($product): bool
     {
-        if (!Validators::isInRange($product->price, [0, 100000000]) || Validators::isNone($product->price)) {
-            $_SESSION['price_error'] = 'Giá sản phẩm không được để trống và phải nhỏ hơn 100000000';
+        if (!strlen($product->name) || strlen($product->name) > 50) {
+            $_SESSION['name_error'] = 'Tên sản phẩm không được để trống và tối đa 50 ký tự';
         }
 
-        if (!Validators::isInRange(strlen($product->name), [1, 50])) {
-            $_SESSION['name_error'] = 'Tên sản phẩm không được để trống và phải nhỏ hơn 50';
+        if (!filter_var(
+            $product->price,
+            FILTER_VALIDATE_INT,
+            ['options' => [
+                'min_range' => 0,
+                'max_range' => 1000000000,
+                'default' => 0
+                ]
+            ]
+        )) {
+            $_SESSION['price_error'] = 'Giá sản phẩm phải là số nguyên dương và nhỏ hơn 1 tỷ';
         }
 
-        if (!Validators::isInRange(strlen($product->image), [0, 255])) {
-            $_SESSION['description_error'] = 'Đường dẫn ảnh sản phẩm phải nhỏ hơn 255';
+        if (!strlen($product->description) > 255) {
+            $_SESSION['description_error'] = 'Mô tả sản phẩm tối đa 255 ký tự';
         }
 
-        if (!Validators::isInRange(strlen($product->description), [0, 255])) {
-            $_SESSION['description_error'] = 'Mô tả sản phẩm phải nhỏ hơn 255';
+        if (!strlen($product->image) > 255) {
+            $_SESSION['image_error'] = 'Đường dẫn ảnh sản phẩm tối đa 255 ký tự';
         }
 
         if (isset($_SESSION['price_error'])
